70272c0891db4600d588ad1837097ab9e33d41a5
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-12-11 05:47:04 +0100

    fix(typo): tuxonaute not tuxaunote

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { NotesModule } from "./notes/note.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
     imports: [
@@ -10,13 +11,14 @@ import { NotesModule } from "./notes/note.module";
             type: (process.env.MARIADB_TYPE as any) || "mariadb",
             host: process.env.MARIADB_HOST || "db",
             port: parseInt(process.env.MARIADB_PORT) || 3306,
-            database: process.env.MARIADB_DB || "tuxaunote_db",
-            username: process.env.MARIADB_USERNAME || "tuxaunote",
+            database: process.env.MARIADB_DB || "tuxonaute_db",
+            username: process.env.MARIADB_USERNAME || "tuxonaute",
             password: process.env.MARIADB_PASSWORD || "password",
             entities: ["dist/**/*.entity{.ts,.js}"],
             synchronize: true,
         }),
         NotesModule,
+        AuthModule,
     ],
     controllers: [AppController],
     providers: [AppService],
