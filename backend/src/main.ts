771928a49e513f6818535b0b15239d20a8155e06
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2024-01-08 22:05:10 +0100

    fix(docker): always restart

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { NestFactory } from "@nestjs/core";
 import { VersioningType } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
